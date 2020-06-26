0a0fa412ebe552b620624bfa669aedaf9fcb2774
Munim89 <64653488+Munim89@users.noreply.github.com>
2020-06-26 13:18:38 +0600

    Create main.py

---
+++ Website/main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, url_for, redirect, session, request
+from client import Client
+
+NAME_KEY = "name"
+
+app = Flask(__name__)
+app.secret_key = "hello"
+
+app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        session[NAME_KEY] = request.form['inputName']
+        return redirect(url_for("home"))
+
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def  logout():
+    session.pop(NAME_KEY, None)
+    return redirect(url_for("login"))
+
+@app.route("/")
+def home():
+    if NAME_KEY not in session:
+       return redirect(url_for("home"))
+
+    return render_template("index.html")
+
+@app.route("/run")
+def run():
+    print("clicked")
+    return "none"
+
+if __name__ == "__main__":
+    app.run(debug=True)
